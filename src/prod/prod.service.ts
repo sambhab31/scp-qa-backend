6570d71b8c93552a2c4b451ecae22b1af41d4270
Sanjib <sanjibraj169@gmail.com>
2021-09-19 22:48:29 +0545

    submit-review endpoint

--- src/prod/prod.service.ts
+++ src/prod/prod.service.ts
@@ -10,9 +10,9 @@ export class ProdService {
 
   async submitReview(review: SubmitReview) {
     try {
-      await this.reviews.queryRunner.query(
-        `update video_information set is_reviewd = true where video_hash = ${review.video_hash}`,
-      );
+      // await this.reviews.manager.query(
+      //   `update video_information set is_reviewd = true where video_hash = ${review.video_hash}`,
+      // );
       return await this.reviews.save(review);
     } catch (error) {
       throw Error(error);
@@ -25,11 +25,6 @@ export class ProdService {
       let query = 'select * from video_information';
       query += ` where last_changed_date_time > '${filter.startDate}'::date`;
       query += ` and last_changed_date_time < '${filter.endDate}'::date`;
-      // if (filter.reviewed) {
-      //   query += ' where is_reviewed = true';
-      // } else {
-      //   query += ' where is_reviewed = false';
-      // }
       query += ' and is_processed = true';
       query += ' order by last_changed_date_time';
       query += filter.sortOrder == 'desc' ? ' desc' : ' asc';
